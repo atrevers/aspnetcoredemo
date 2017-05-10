a2b827d08cf6702a4ab38353f7b74a65fbd16d75
Andrew Trevers <andrew.trevers@potterclarkson.com>
2017-05-10 17:35:13 +0100

    Implemented adding stop

--- wwwroot/js/tripEditorController.js
+++ wwwroot/js/tripEditorController.js
@@ -12,8 +12,11 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function (response) {
                 // success
                 angular.copy(response.data, vm.stops);
@@ -26,6 +29,25 @@
                 vm.isBusy = false;
             });
 
+        vm.addStop = function () {
+
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+                    // success
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function (error) {
+                    // failure
+                    vm.errorMessage = "Failed to add new stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+
+        }
     }
 
     function _showMap(stops) {
